84eb327b6f7fae121f8c52aa47fe2a89881e44ff
Jon Paniagua <crazer1@gmail.com>
2026-04-29 17:38:39 -0400

    Refactor test-api.ps1 for improved readability

--- test-api.ps1
+++ test-api.ps1
@@ -1,6 +1,9 @@
-$server = "<your-horizon-server>"
+$server   = "your-horizon-server"
+$domain   = "your-netbios-domain"
+$username = "your-username"
+$password = "your-password"
+$testMachine = "yourvdi.domain.fqdn"
 
-# Bypass self-signed cert
 Add-Type @"
     using System.Net;
     using System.Security.Cryptography.X509Certificates;
@@ -11,27 +14,28 @@ Add-Type @"
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCerts2
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 
-$pathsToTry = @(
-    "/rest/swagger.json",
-    "/rest/api-docs",
-    "/rest/v1/swagger.json",
-    "/rest/v2/swagger.json",
-    "/rest/monitor/v1/events",
-    "/rest/monitor/v2/events",
-    "/rest/external/v1/audit-events",
-    "/rest/inventory/v1/machines",
-    "/rest/inventory/v2/machines",
-    "/portal/rest/swagger.json",
-    "/admin/rest/swagger.json",
-    "/broker/rest/swagger.json"
-)
+$loginUri = "https://" + $server + "/rest/login"
+$authBody = @{ domain = $domain; username = $username; password = $password } | ConvertTo-Json
+$token    = (Invoke-RestMethod -Uri $loginUri -Method POST -ContentType "application/json" -Body $authBody).access_token
+$headers  = @{ Authorization = "Bearer " + $token }
 
-foreach ($path in $pathsToTry) {
+# Test various server-side filter parameter styles
+$baseUri = "https://" + $server + "/rest/external/v1/audit-events"
+
+$tests = [ordered]@{
+    "type filter"         = $baseUri + "?page=1&size=5&type=BROKER_USERLOGGEDIN"
+    "machine_dns_name"    = $baseUri + "?page=1&size=5&machine_dns_name=" + $testMachine
+    "filter by type"      = $baseUri + "?page=1&size=5&filter=type%3DBROKER_USERLOGGEDIN"
+    "event_type param"    = $baseUri + "?page=1&size=5&event_type=BROKER_USERLOGGEDIN"
+    "module param"        = $baseUri + "?page=1&size=5&module=BROKER"
+}
+
+foreach ($test in $tests.GetEnumerator()) {
     try {
-        $resp = Invoke-WebRequest -Uri "https://$server$path" -Method GET -UseBasicParsing -ErrorAction Stop
-        Write-Host "OK  [$($resp.StatusCode)] $path" -ForegroundColor Green
+        $resp = Invoke-RestMethod -Uri $test.Value -Method GET -Headers $headers -ErrorAction Stop
+        Write-Host "OK  [$($resp.Count) results] $($test.Key)" -ForegroundColor Green
     } catch {
         $code = $_.Exception.Response.StatusCode.value__
-        Write-Host "    [$code] $path" -ForegroundColor Gray
+        Write-Host "    [$code] $($test.Key)" -ForegroundColor Gray
     }
 }
