7e2506d97cf172c7e77efacced27d3b327111f1a
Jon Paniagua <crazer1@gmail.com>
2026-04-29 16:36:36 -0400

    Update get-api.ps1

--- get-api.ps1
+++ get-api.ps1
@@ -1,9 +1,8 @@
-$server = "<your-horizon-server>"
-$domain = "<your-netbios-domain>"
-$username = "<your-username>"
-$password = "<your-password>"
+$server   = "your-horizon-server"
+$domain   = "your-netbios-domain"
+$username = "your-username"
+$password = "your-password"
 
-# Bypass self-signed cert
 Add-Type @"
     using System.Net;
     using System.Security.Cryptography.X509Certificates;
@@ -14,16 +13,13 @@ Add-Type @"
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCerts2
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 
-# Authenticate
-$loginUri = "https://$server/rest/login"
+$loginUri  = "https://" + $server + "/rest/login"
 $authBody  = @{ domain = $domain; username = $username; password = $password } | ConvertTo-Json
 $token     = (Invoke-RestMethod -Uri $loginUri -Method POST -ContentType "application/json" -Body $authBody).access_token
 
 $headers = @{ Authorization = "Bearer $token" }
 
-# Fetch the swagger doc
-$swaggerUri = "https://$server/rest/v1/swagger.json"
+$swaggerUri = "https://" + $server + "/rest/v1/swagger.json"
 $swagger    = Invoke-RestMethod -Uri $swaggerUri -Method GET -Headers $headers
 
-# Print all available API paths
 $swagger.paths.PSObject.Properties.Name | Sort-Object
